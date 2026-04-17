7295623bd51902cc4d6c6026bfa2b03a66d662d4
Guree sandhu <113997269+guri5405@users.noreply.github.com>
2026-04-17 12:38:58 +0530

    Add UserContext for global state management
    Created UserContext for global state management.

---
+++ minicontext/src/context/UserContext.js
@@ -0,0 +1,7 @@
+import React, { createContext } from "react";
+
+
+//  created the context for global state management
+const UserContext = React.createContext();
+
+export default UserContext;// here we create the store for the data
